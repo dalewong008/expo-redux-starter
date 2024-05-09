9ac85b33c4ba0712337a727c35cd089f91a93220
dalewong008 <dalewong008@gmail.com>
2024-05-10 01:34:54 +1000

    init commit

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,39 @@
+import { Image, StyleSheet, Platform, View, Text } from 'react-native';
+
+import { HelloWave } from '@/components/HelloWave';
+import ParallaxScrollView from '@/components/ParallaxScrollView';
+import { ThemedText } from '@/components/ThemedText';
+import { ThemedView } from '@/components/ThemedView';
+
+import { Provider } from 'react-redux';
+import store from '../../store';
+import Counter from '@/components/Counter';
+
+export default function HomeScreen() {
+  return (
+    <View>
+      <Text>
+        This is me.
+      </Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  titleContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 8,
+  },
+  stepContainer: {
+    gap: 8,
+    marginBottom: 8,
+  },
+  reactLogo: {
+    height: 178,
+    width: 290,
+    bottom: 0,
+    left: 0,
+    position: 'absolute',
+  },
+});
